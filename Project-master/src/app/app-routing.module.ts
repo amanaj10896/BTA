95328631424cf9ab029679c4c9c9b92f0b23669a
amanaj10896 <amanaj786@gmail.com>
2018-08-23 17:12:32 +0530

    m

---
+++ Project-master/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+/*import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+@NgModule({
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
+
+import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: AppComponent }
+];
+*/
